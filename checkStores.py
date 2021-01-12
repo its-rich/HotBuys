32ba061077a96f0b9f9d3672d36e9c00f2b42dad
Richard Liu <41549414+its-rich@users.noreply.github.com>
2021-01-12 20:12:03 +1100

    All individual web scrapers completed & refined

--- checkStores.py
+++ checkStores.py
@@ -1,57 +1,94 @@
+def checkSimilarity(productName, productCreator, query, maxSimilarity):
+    similarity = textdistance.levenshtein.normalized_similarity(productName.lower(), query["name"])
+
+    if similarity < 0.5:
+        return False
+    elif query["creator"] != "":
+        similarity += textdistance.levenshtein.normalized_similarity(productName.lower(), query["creator"])
+
+    if maxSimilarity <= similarity:
+        return similarity
+    else:
+        return False
+
+def createProductJSON(store, productName, productCreator, price, link):
+    json = {
+        "store": store,
+        "name": productName,
+        "creator": productCreator,
+        "price": price,
+        "link": link
+    }
+    return json
+
+payload = []
+
+query = {
+    "name": "7 ways",
+    "creator": "jamie oliver"
+}
+
+########filter out productCreator name in productName
+
 import requests
 import textdistance
+from splinter import Browser
 from bs4 import BeautifulSoup
 
-kmartBooks = {}
+kmartProducts = {}
 
-headers = requests.utils.default_headers()
-headers.update({
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
-})
+url = f"https://www.kmart.com.au/webapp/wcs/stores/servlet/SearchDisplay?searchTerm={query['name']}&categoryId=&storeId=10701&catalogId=10102&langId=-1&beginIndex=0&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=#.plp-wrapper"
+product = createProductJSON("Kmart", "", "", "", url)
+maxSimilarity = 0
 
-headers = {
-   'accept':'*/*',
-    'accept-encoding':'gzip, deflate, br',
-    'accept-language':'en-GB,en;q=0.9,en-US;q=0.8,hi;q=0.7,la;q=0.6',
-    'cache-control':'no-cache',
-    'dnt':'1',
-    'pragma':'no-cache',
-    'referer':'https://www.google.com/',
-    'sec-fetch-mode':'no-cors',
-    'sec-fetch-site':'cross-site',
-    'user-agent':"Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
- }
+# Since Kmart has the anti-scraping software by Incapsula the splinter library
+# must be used to collect the HTML
 
-query = 'jk rowling'
-url = f"https://www.kmart.com.au/webapp/wcs/stores/servlet/SearchDisplay?searchTerm={query}&categoryId=&storeId=10701&catalogId=10102&langId=-1&beginIndex=0&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=#.plp-wrapper"
-url = "https://www.kmart.com.au/"
+browser = Browser("chrome", **executable_path ,incognito=True)
+browser.visit(url)
 
-a = requests.Session()
-a.get(url)
+soup = BeautifulSoup(browser.html, "lxml")
 
-request = a.get(url, headers=headers)
-soup = BeautifulSoup(request.content, "html.parser")
-print(soup)
-for book in soup.findAll("div", {"class": "product product_box small-6 medium-4 large-4 columns clearfix col "}):
-    print(book.find("div", {"class": "product-details"}))
-    # print(book.find("p", {"class": "title"}))
-    break
+for tag in soup.findAll("div", {"class": "product-details"}):
+    productCreator = ""
+    productName = tag.find("p", {"class": "title"}).text.strip()
+    price = tag.find("span", {"class": "price"}).text.strip()
+    link = "https://www.kmart.com.au" + tag.find_parent("div").find("a")["href"].strip()
 
-########filter out author name in title
+    if " - Book" in productName:
+        productName = productName.replace(" - Book", "")
+
+    if "by" in productName:
+        metadata = productName.split(" by ")
+        productCreator = metadata.pop().strip()
+        productName = " by ".join(metadata).strip()
+
+    updateSimilarity = checkSimilarity(productName, productCreator, query, maxSimilarity)
+    if updateSimilarity:
+        maxSimilarity = updateSimilarity
+        product = createProductJSON("Kmart", productName, productCreator, price, link)
+
+    kmartProducts[productName] = (price, link)
+
+browser.quit()
+payload.append(product)
+
+
 
 # import requests
 # import textdistance
 # from bs4 import BeautifulSoup
 #
-# davidJonesBooks = {}
+# davidJonesProducts = {}
+#
+# url = f"https://search.www.davidjones.com/search?w={query['name']}"
+# product = createProductJSON("David Jones", "", "", "", url)
+# maxSimilarity = 0
 #
 # headers = requests.utils.default_headers()
 # headers.update({
 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 # })
-#
-# query = 'jamie oliver'
-# url = f"https://search.www.davidjones.com/search?w={query}"
 #
 # request = requests.get(url, headers=headers)
 # soup = BeautifulSoup(request.content, "lxml")
@@ -60,7 +97,6 @@ for book in soup.findAll("div", {"class": "product product_box small-6 medium-4 
 #
 # for styleNumber in soup.findAll("p", {"class": "style-number"}):
 #     headers.update({
-#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
 #         "referer": url
 #     })
 #     stylesLink += "&SKU=" + styleNumber.text.strip().split(" ")[2].strip()
@@ -68,14 +104,21 @@ for book in soup.findAll("div", {"class": "product product_box small-6 medium-4 
 # request = requests.get(stylesLink, headers=headers)
 # soup = BeautifulSoup(request.text, "lxml")
 #
-# for book in soup.findAll("div", {"class": '\\"item-detail\\"'}):
-#     title = book.find("a").text.strip()
-#     price = book.find("span", {"itemprop": '\\"price\\"'}).text.strip()
+# for tag in soup.findAll("div", {"class": '\\"item-detail\\"'}):
+#     productCreator = tag.find("div", {"class": '\\"item-brand\\"'}).text.strip()
+#     productName = tag.find("a").text.strip()
+#     price = tag.find("span", {"itemprop": '\\"price\\"'}).text.strip()
+#     link = tag.find("a")["href"].replace("\\", "")
+#     link = link.replace("\"", "").strip()
 #
-#     davidJonesBooks[title] = price
+#     updateSimilarity = checkSimilarity(productName, productCreator, query, maxSimilarity)
+#     if updateSimilarity:
+#         maxSimilarity = updateSimilarity
+#         product = createProductJSON("Target", productName, productCreator, price, link)
 #
-# for key, value in davidJonesBooks.items():
-#     print(key + " " + value)
+#     # davidJonesProducts[link] = (productName, productCreator, price)
+#
+# payload.append(product)
 
 
 
@@ -83,37 +126,43 @@ for book in soup.findAll("div", {"class": "product product_box small-6 medium-4 
 # import textdistance
 # from bs4 import BeautifulSoup
 #
-# bigwBooks = {}
+# bigwProducts = {}
+#
+# url = f"https://www.bigw.com.au/search?pageSize=144&q={query['name']}%3Arelevance#"
+# product = createProductJSON("Big W", "", "", "", url)
+# maxSimilarity = 0
 #
 # headers = requests.utils.default_headers()
 # headers.update({
 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 # })
 #
-# query = 'jamie oliver'
-# url = f"https://www.bigw.com.au/search?pageSize=144&q={query}%3Arelevance#"
-#
 # request = requests.get(url, headers=headers)
-# soup = BeautifulSoup(request.content, "html.parser")
+# soup = BeautifulSoup(request.content, "lxml")
 #
-# # It doesn't display authors so this assumes every product relates to the author
+# # It doesn't display productCreators so this assumes every product relates to the productCreator
 # # thus only 1 dict is necessary
-# for book in soup.findAll("div", {"class": "productDescription"}):
-#     title = book.find("a").text.strip()
+# for tag in soup.findAll("div", {"class": "productDescription"}):
+#     productName = tag.find("a").text.strip()
+#     link = "https://www.bigw.com.au" + tag.find("h4").find("a")["href"].strip()
 #
 #     # The price is split up across 3 divs to display:
 #     #               Currency ($) | Dollars | Cents
 #     # So the text values across these divs must be combined
-#     priceDiv = book.find("div", {"class": "online-price padding-right-zero"})
+#     priceDiv = tag.find("div", {"class": "online-price padding-right-zero"})
 #     price = "$" + priceDiv.find("span", {"class": "priceClass"}).text.strip()
 #     cents = priceDiv.find("span", {"class": "priceSuffix"})
 #     if cents is not None:
 #         price += "." + cents.text.strip()
 #
-#     bigwBooks[title] = price
+#     updateSimilarity = checkSimilarity(productName, "", query, maxSimilarity)
+#     if updateSimilarity:
+#         maxSimilarity = updateSimilarity
+#         product = createProductJSON("Big W", productName, "", price, link)
 #
-# for key, value in bigwBooks.items():
-#     print(key)
+#    # bigwProducts[link] = (productName, "", price)
+#
+# payload.append(product)
 
 
 
@@ -121,112 +170,118 @@ for book in soup.findAll("div", {"class": "product product_box small-6 medium-4 
 # import textdistance
 # from bs4 import BeautifulSoup
 #
-# targetBooks = {}
+# targetProducts = {}
+#
+# url = f"https://www.target.com.au/search?text={query['name']}"
+# product = createProductJSON("Target", "", "", "", url)
+# maxSimilarity = 0
 #
 # headers = requests.utils.default_headers()
 # headers.update({
 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 # })
-#
-# query = 'jamie oliver'
-# url = f"https://www.target.com.au/search?text={query}"
 #
 # request = requests.get(url, headers=headers)
-# soup = BeautifulSoup(request.content, "html.parser")
+# soup = BeautifulSoup(request.content, "lxml")
 #
-# for book in soup.findAll("div", {"class": "detail"}):
+# for tag in soup.findAll("div", {"class": "detail"}):
+#     link = "https://www.target.com.au" + tag.find("a")["href"].strip()
+#     productName = ""
+#     productCreator = ""
 #
-#     if "-" in book.find("a")["title"].strip():
-#         metadata = book.find("a")["title"].strip().split("-")
-#         author = metadata.pop().strip()
-#         title = "-".join(metadata).strip()
+#     if "-" in tag.find("a")["title"].strip():
+#         metadata = tag.find("a")["title"].strip().split("-")
+#         productCreator = metadata.pop().strip()
+#         productName = "-".join(metadata).strip()
 #     else:
-#         pass
-#     price = book.find("span", {"class": "price-regular price"}).text.strip()
+#         ## TODO:
+#         print(tag.find("a")["title"].strip())
+#     price = tag.find("span", {"class": "price-regular price"}).text.strip()
 #
-#     if author in targetBooks:
-#         targetBooks[author][title] = price
-#     else:
-#         targetBooks[author] = {title: price}
+#     updateSimilarity = checkSimilarity(productName, productCreator, query, maxSimilarity)
+#     if updateSimilarity:
+#         maxSimilarity = updateSimilarity
+#         product = createProductJSON("Target", productName, productCreator, price, link)
 #
-# for book in targetBooks['Jamie Oliver']:
-#     print(targetBooks['Jamie Oliver'][book])
-#     if textdistance.levenshtein.normalized_similarity(book.lower(), 'Harry Potter and the Half-Blood Prince') > 0.8:
-#         print(price)
+#    # targetProducts[link] = (productName, productCreator, price)
+#
+# payload.append(product)
 
 
 # import requests
 # import textdistance
 # from bs4 import BeautifulSoup
 #
-# authorSimilarity = 1
 # pageNum = 1
-# dymocksBooks = {}
+# dymocksProducts = {}
+#
+# product = createProductJSON("Dymocks", "", "", "", "")
+# maxSimilarity = 1
 #
 # headers = requests.utils.default_headers()
 # headers.update({
 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 # })
 #
-# query = "j.k. rowling"
+# while (maxSimilarity > 0.8):
+#     maxSimilarity = 0
+#     request = requests.get(f"https://www.dymocks.com.au/books/page-{pageNum}/?term={query['name']}&npp=96", headers=headers)
+#     soup = BeautifulSoup(request.content, "lxml")
 #
-# while (authorSimilarity > 0.8):
-#     authorSimilarity = 0
-#     request = requests.get(f"https://www.dymocks.com.au/books/page-{pageNum}/?term={query}&npp=96", headers=headers)
-#     soup = BeautifulSoup(request.content, "html.parser")
+#     for tag in soup.findAll("div", {"class": "product-tile"}):
+#         productCreator = tag.find("div", {"class": "product-tile-desc product-tile-author"})["title"].strip()
+#         productName = tag.find("a", {"class": "product-tile-title product-title"})["title"].strip()
+#         link = "https://www.dymocks.com.au" + tag.find("a", {"class": "product-tile-title product-title"})["href"].strip()
+#         price = tag.find("div", {"class": "product-tile-price"}).text.strip()
 #
-#     for book in soup.findAll("div", {"class": "product-tile"}):
-#         author = book.find("div", {"class": "product-tile-desc product-tile-author"})["title"].strip()
-#         title = book.find("a", {"class": "product-tile-title product-title"})["title"].strip()
-#         price = book.find("div", {"class": "product-tile-price"}).text.strip()
+#         updateSimilarity = checkSimilarity(productName, productCreator, query, maxSimilarity)
+#         if updateSimilarity:
+#             maxSimilarity = updateSimilarity
+#             print(price)
+#             if product["price"] == "":
+#                 product = createProductJSON("Myer", productName, productCreator, price, link)
+#             elif product["price"] > price:
+#                 product = createProductJSON("Myer", productName, productCreator, price, link)
 #
-#         similarity = textdistance.levenshtein.normalized_similarity(author.lower(), query)
-#         if similarity > authorSimilarity:
-#             authorSimilarity = similarity
-#
-#         if author in dymocksBooks:
-#             dymocksBooks[author][title] = price
-#         else:
-#             dymocksBooks[author] = {title: price}
+#         # dymocksProducts[link] = (productName, productCreator, price)
 #
 #     pageNum += 1
 #
-# for book in dymocksBooks['J.K. Rowling']:
-#     if textdistance.levenshtein.normalized_similarity(book.lower(), 'Harry Potter and the Half-Blood Prince') > 0.8:
-#         print(dymocksBooks['J.K. Rowling'][book])
-
+# payload.append(product)
 
 # import requests
 # import textdistance
 # from bs4 import BeautifulSoup
 #
-# myerBooks = {}
-# query = "J.K. Rowling"
+# myerProducts = {}
+#
+# url = f"https://www.myer.com.au/search?query={query['name']}"
+# product = createProductJSON("Myer", "", "", "", url)
+# maxSimilarity = 0
 #
 # headers = requests.utils.default_headers()
 # headers.update({
 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 # })
 #
-# request = requests.get(f"https://www.myer.com.au/search?query={query}", headers=headers)
-# soup = BeautifulSoup(request.content, "html.parser")
+# request = requests.get(url, headers=headers)
+# soup = BeautifulSoup(request.content, "lxml")
 #
-# for book in soup.findAll("div", {"class": "css-m4cs32"}):
-#     author = book.find("span", {"data-automation": "product-brand"}).text.strip()
-#     title = book.find("span", {"data-automation": "product-name"}).text.strip()
-#     if book.find("p", {"data-automation": "product-price-now"}) is not None:
-#         price = book.find("p", {"data-automation": "product-price-now"}).findAll("span")[1].text.strip()
+# for tag in soup.findAll("div", {"class": "css-m4cs32"}):
+#     productCreator = tag.find("span", {"data-automation": "product-brand"}).text.strip()
+#     productName = tag.find("span", {"data-automation": "product-name"}).text.strip()
+#     link = "https://www.myer.com.au" + tag.find("a")["href"].strip()
+#
+#     if tag.find("p", {"data-automation": "product-price-now"}) is not None:
+#         price = tag.find("p", {"data-automation": "product-price-now"}).findAll("span")[1].text.strip()
 #     else:
-#         price = book.find("p", {"data-automation": "product-price-was"}).findAll("span")[1].text.strip()
+#         price = tag.find("p", {"data-automation": "product-price-was"}).findAll("span")[1].text.strip()
 #
-#     if author in myerBooks:
-#         myerBooks[author][title] = price
-#     else:
-#         myerBooks[author] = {title: price}
+#     updateSimilarity = checkSimilarity(productName, productCreator, query, maxSimilarity)
+#     if updateSimilarity:
+#         maxSimilarity = updateSimilarity
+#         product = createProductJSON("Myer", productName, productCreator, price, link)
 #
+#     # myerProducts[link] = (productName, productCreator, price)
 #
-# for book in myerBooks['J.K. Rowling']:
-#     print(myerBooks['J.K. Rowling'][book])
-#     # print(book + str(textdistance.levenshtein.normalized_similarity(book.lower(), 'Harry Potter and the Half-Blood Prince')))
-#     if textdistance.levenshtein.normalized_similarity(book.lower(), 'Harry Potter and the Half-Blood Prince') > 0.8:
-#         print(myerBooks['J.K. Rowling'][book])
+# payload.append(product)
